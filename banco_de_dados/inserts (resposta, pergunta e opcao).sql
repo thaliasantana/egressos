0a5c7954520b21bd9c0da51d418f67b6234cdda0
Lucianodcb <30538692+Lucianodcb@users.noreply.github.com>
2018-11-15 14:02:45 -0200

    Fim preenchimeno dados do Banco

--- banco_de_dados/inserts (resposta, pergunta e opcao).sql
+++ banco_de_dados/inserts (resposta, pergunta e opcao).sql
@@ -3,18 +3,26 @@ INSERT INTO pais (NomePais) VALUES ("Brasil");
 INSERT INTO estado (nomeEstado, idPais) VALUES ("Goiás", 1);
 
 INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Ceres", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Uruana", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Rubitaba", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("São Patrício", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Carmo do Rio Verde", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Rialma", 1);
+INSERT INTO cidade (nomeCidade, idEstado) VALUES ("Nova Glória", 1);
 
 INSERT INTO curso (nomeCurso) VALUES ("Técnico Agrícola");
-INSERT INTO curso (nomeCurso) VALUES ("Técnico Informática para Internet Integrado a Ensino Médio");
-INSERT INTO curso (nomeCurso) VALUES ("Técnico Meio Ambiente Integrado a Ensino Médio");
-INSERT INTO curso (nomeCurso) VALUES ("Técnico Agropecuária Integrado a Ensino Médio");
+INSERT INTO curso (nomeCurso) VALUES ("Técnico em Informática para Internet");
+INSERT INTO curso (nomeCurso) VALUES ("Técnico em Meio Ambiente");
+INSERT INTO curso (nomeCurso) VALUES ("Técnico em Agropecuária");
+INSERT INTO curso (nomeCurso) VALUES ("Técnico em Administração");
+INSERT INTO curso (nomeCurso) VALUES ("Técnico em Informática");
 INSERT INTO curso (nomeCurso) VALUES ("Bacharelado em Sistemas de Informação");
 INSERT INTO curso (nomeCurso) VALUES ("Bacharelado em Zootecnia");
 INSERT INTO curso (nomeCurso) VALUES ("Bacharelado em Agronomia");
 INSERT INTO curso (nomeCurso) VALUES ("Licenciatura em Ciências Biolôgicas");
 INSERT INTO curso (nomeCurso) VALUES ("Licenciatura em Química");
-INSERT INTO curso (nomeCurso) VALUES ("Pós-graduação Ensino em Ciências da Natureza");
-INSERT INTO curso (nomeCurso) VALUES ("Pós-graduação Matemática");
+INSERT INTO curso (nomeCurso) VALUES ("Especilização em Ensino de Ciências da Natureza");
+INSERT INTO curso (nomeCurso) VALUES ("Especialização em Educação Matemática");
 INSERT INTO curso (nomeCurso) VALUES ("Mestrado em Irrigação do Cerrado");
 
 select * from egresso;
@@ -216,7 +224,8 @@ INSERT INTO egresso VALUES (null, "Marcelo Torres Ramos de Andrade", "8972466661
 "M", "Rua Santa Tereza", "2081", "Caxias", "Casa", 1);
 INSERT INTO egresso VALUES (null, "Pedro Freixinho Nunes", "89728885667", "1955-12-29", "Pedronunes@gmail.com", "62945475563", "62945475563", 
 "M", "Rua D", "2870", "Ipia", "Casa", 1);
-
+INSERT INTO egresso VALUES (null, "Aparecida Nunes", "89732895667", "1965-12-29", "aparecidan@gmail.com", "62940005563", "62940005563", 
+"F", "Rua P", "870", "Novo", "Casa", 1);
 
 
 INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
@@ -241,6 +250,186 @@ INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCur
 VALUES (2011, 2008, 2, 10);
 INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
 VALUES (1999, 1996, 4, 11);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2000, 1997, 8, 12);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1998, 1994, 2, 13);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1996, 1993, 4, 14);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2003, 2000, 8, 15);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2010, 2, 16);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1999, 1996, 1, 17);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2004, 2005, 8, 18);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2012, 2008, 2, 19);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1998, 1996, 4, 20);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2000, 1998, 2, 21);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1997, 1995, 4, 22);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2000, 1997, 2, 23);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1997, 1993, 1, 24);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1996, 1994, 4, 25);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2000, 1994, 2, 26);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1997, 1994, 1, 27);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1998, 1994, 4, 28);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2000, 1996, 2, 29);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1997, 1993, 1, 30);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1999, 1995, 1, 31);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1999, 1994, 7, 32);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2000, 1995, 6, 33);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (1996, 1993, 1, 34);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2004, 1999, 8, 35);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2012, 7, 36);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2014, 2009, 6, 37);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2013, 2009, 9, 38);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2009, 2005, 8, 39);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2013, 2010, 7, 40);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2014, 6, 41);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2013, 9, 42);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2011, 4, 43);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2010, 3, 44);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2013, 1, 45);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2014, 9, 46);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2012, 4, 47);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2009, 3, 48);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2012, 2, 49);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2014, 3, 50);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2010, 2005, 4, 51);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2010, 3, 52);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2016, 2013, 1, 53);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2014, 7, 54);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2009, 4, 55);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2009, 6, 56);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2012, 1, 57);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2013, 10, 58);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2004, 4, 59);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2010, 13, 60);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2012, 13, 61);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2013, 11, 62);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2008, 2005, 12, 63);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2009, 13, 64);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2013, 12, 65);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2012, 11, 66);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2004, 2, 67);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2010, 9, 68);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2016, 2012, 13, 69);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2013, 11, 70);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2003, 2, 71);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2010, 9, 72);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2012, 13, 73);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2012, 11, 74);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2002, 4, 75);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2011, 9, 76);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2014, 12, 77);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2011, 11, 78);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2004, 2, 79);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2011, 9, 80);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2013, 10, 81);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2012, 11, 82);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2005, 6, 83);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2014, 2010, 9, 84);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2016, 2012, 13, 85);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2012, 1, 86);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2017, 2013, 2, 87);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2011, 9, 88);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2015, 2012, 13, 89);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2012, 3, 90);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2008, 2004, 2, 91);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2011, 9, 92);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2011, 13, 93);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2011, 11, 94);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2010, 2007, 2, 95);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2013, 2010, 9, 96);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso)
+VALUES (2017, 2013, 13, 97);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2016, 2011, 7, 98);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2007, 2002, 2, 99);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2015, 2011, 9, 100);
+INSERT INTO cursoConcluido (anoFimCursoConcluido, anoInicioCursoConcluido, idCurso, idEgresso) 
+VALUES (2000, 1998, 1, 74);
 
 /* Aqui começa de fato todas as inserções referentes ao questionário */
 
