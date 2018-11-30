0d161bb2479f97bdc2cfa8df64332fd046fbadf1
Ianka Talita <30481954+iankatalitaa@users.noreply.github.com>
2018-11-30 15:21:19 -0200

    atualização de novo brazel

---
+++ banco_de_dados/inserts/tabelaUsuario.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "claudinha32@hotmail.com", "10203040");
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "larissa17@hotmail.com", "102030");
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "iara87@hotmail.com", "123456789");
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "ilana78@hotmail.com", "102564");
