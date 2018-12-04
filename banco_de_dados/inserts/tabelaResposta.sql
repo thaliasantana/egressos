70137f5c65ef2fd534eade63a74b2cbf175d9c87
thaliasantana <thaliassantana15@gmail.com>
2018-12-04 10:25:12 -0200

    Inserção de tabela Resposta e update Usuario

---
+++ banco_de_dados/inserts/tabelaResposta.sql
@@ -0,0 +1,10 @@
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 1, 1, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 2, 7, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 3, 13, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 4, 19, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 5, 22, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 6, 25, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 7, 27, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 8, 30, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 9, 32, 2018);
+INSERT INTO resposta (idEgresso, idPergunta, idOpcao, anoResposta) VALUES (1, 10, 37, 2018);
--- banco_de_dados/inserts/tabelaUsuario.sql
+++ banco_de_dados/inserts/tabelaUsuario.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "claudinha32@hotmail.com", "10203040");
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "larissa17@hotmail.com", "102030");
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "iara87@hotmail.com", "123456789");
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "ilana78@hotmail.com", "102564");
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "claudinha32@hotmail.com", md5("10203040"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "larissa17@hotmail.com", md5("102030"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "iara87@hotmail.com", md5("123456789"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "ilana78@hotmail.com", md5("102564"));
