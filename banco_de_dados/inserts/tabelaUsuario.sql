a969a871223e9482de853a33959e319dd32c7571
thaliasantana <thaliassantana15@gmail.com>
2018-12-07 10:29:54 -0200

    arrumando nome do campo usuário

--- banco_de_dados/inserts/tabelaUsuario.sql
+++ banco_de_dados/inserts/tabelaUsuario.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "claudinha32@hotmail.com", md5("10203040"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "larissa17@hotmail.com", md5("102030"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "iara87@hotmail.com", md5("123456789"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES (null, "ilana78@hotmail.com", md5("102564"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("claudinha32@hotmail.com", md5("10203040"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("larissa17@hotmail.com", md5("102030"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("iara87@hotmail.com", md5("123456789"));
+INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("ilana78@hotmail.com", md5("102564"));
