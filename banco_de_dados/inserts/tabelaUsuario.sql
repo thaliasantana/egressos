c87f2c9fa324fa61becdcf026631213624cf2eeb
Ianka Talita <30481954+iankatalitaa@users.noreply.github.com>
2018-12-07 10:50:44 -0200

    atualização

--- banco_de_dados/inserts/tabelaUsuario.sql
+++ banco_de_dados/inserts/tabelaUsuario.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("claudinha32@hotmail.com", md5("10203040"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("larissa17@hotmail.com", md5("102030"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("iara87@hotmail.com", md5("123456789"));
-INSERT INTO usuario (emailUsuario, senhaUsuario) VALUES ("ilana78@hotmail.com", md5("102564"));
+INSERT INTO usuario (nomeUsuario, senhaUsuario) VALUES ("claudinha32@hotmail.com", md5("10203040"));
+INSERT INTO usuario (nomeUsuario, senhaUsuario) VALUES ("larissa17@hotmail.com", md5("102030"));
+INSERT INTO usuario (nomeUsuario, senhaUsuario) VALUES ("iara87@hotmail.com", md5("123456789"));
+INSERT INTO usuario (nomeUsuario, senhaUsuario) VALUES ("ilana78@hotmail.com", md5("102564"));
